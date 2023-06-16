e4e187fccda8fb3a37e760dd4ab51c3d811d9435
Didrik Munther <dmu0817@gmail.com>
2023-06-16 14:02:32 +0200

    Initial Commit

---
+++ BonShop/BonShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonShopApp.swift
+//  BonShop
+//
+//  Created by Didrik Munther on 2023-06-16.
+//
+
+import SwiftUI
+
+@main
+struct BonShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
